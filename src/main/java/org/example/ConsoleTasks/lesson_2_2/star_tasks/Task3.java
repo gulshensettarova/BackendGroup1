8ef6188154e90dfe898af33fbb621966fb5ffb08
gulshensettarova <gulshensettarova@gmail.com>
2024-10-09 21:41:02 +0400

    Added 3 Star tasks

---
+++ src/main/java/org/example/ConsoleTasks/lesson_2_2/star_tasks/Task3.java
@@ -0,0 +1,32 @@
+package org.example.ConsoleTasks.lesson_2_2.star_tasks;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+//                *
+//              * * *
+//            * * * * *
+//          * * * * * * *
+//        * * * * * * * * *
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int k=1;
+        int number = sc.nextInt();
+        for (int i = 1; i <= number; i++) {
+            for(int j = 1; j <= number-i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= k; j++) {
+                System.out.print("*");
+            }
+            k+=2;
+            for(int j = 1; j <= number-i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
